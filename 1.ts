8cc93ff899e4c31751f769405a00f1396817c373
Daniel Jackoway <commits@danieljackoway.com>
2021-12-01 20:00:20 -0800

    Day 1

--- 1.ts
+++ 1.ts
@@ -1,22 +1,35 @@
-import { readLines } from './util';
+import { readInts } from './util';
 
 const FILENAME = "1.txt";
 
 function readFile() {
-  return readLines(FILENAME);
+  return readInts(FILENAME);
 }
 
 function part1() {
   const lines = readFile();
+  let ct = 0;
+  let prev = -1;
+  for (let l of lines) {
+    if (prev !== -1 && prev < l) {
+      ct++;
+    }
+    prev = l;
+  }
+  return ct;
 
 
-
-
-  const lines2 = readFile();
 }
 console.log(part1());
 
 function part2() {
   const lines = readFile();
+  let ct = 0;
+  for (let i = 3; i < lines.length; i++) {
+    if (lines[i] > lines[i - 3]) {
+      ct++;
+    }
+  }
+  return ct;
 }
 console.log(part2());
