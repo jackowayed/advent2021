10d1089d7ed82d89c8f15849a9a38f6431e17570
Daniel Jackoway <commits@danieljackoway.com>
2021-12-05 21:21:33 -0800

    6 p2

--- 6.ts
+++ 6.ts
@@ -30,9 +30,47 @@ function part1() {
 
 
 }
-console.log(part1());
+//console.log(part1());
+
+const MEM = new Map();
+
+function key(timer, daysLeft) {
+  return timer + "," + daysLeft;
+}
+
+function endingFish(timer: number, daysLeft: number): number {
+  //console.log(timer, daysLeft);
+  if (daysLeft === 0) return 1;
+  const key_ = key(timer, daysLeft);
+  if (MEM.has(key_)) return MEM.get(key_);
+  let val = 0;
+  if (timer === 0) {
+    //console.log("0");
+    val = endingFish(8, daysLeft - 1) + endingFish(6, daysLeft - 1);
+    //console.log(val);
+  } else {
+    val = endingFish(timer - 1, daysLeft - 1);
+    //console.log(val);
+  }
+  if (val === NaN) {
+    //console.log(timer, daysLeft);
+    throw new Error("NaN");
+  }
+  //console.log(val);
+  MEM.set(key_, val);
+  return val;
+}
 
 function part2() {
-  const lines = readFile();
+  const fish = readFile();
+  //const fish = [5];
+  const mem = new Map();
+  let count = 0;
+  for (let n of fish) {
+    const e = endingFish(n, 256);
+    console.log(e);
+    count += e;
+  }
+  return count;
 }
 console.log(part2());
