cea91e61710b9b155de2a3c9cbf8f1d011b763aa
Daniel Jackoway <commits@danieljackoway.com>
2021-12-08 21:21:29 -0800

    fixed 9 p1

--- 9.ts
+++ 9.ts
@@ -8,18 +8,25 @@ function readFile(): number[][] {
   return readLines(FILENAME).map(line => [...line].map(c => parseInt(c)));
 }
 
-function pointIsLower(y: number, x: number, dy: number, dx: number, grid: Grid) {
+function pointIsLowerOrEqual(y: number, x: number, dy: number, dx: number, grid: Grid, debugArr: number[]) {
   const y_ = y + dy;
   const x_ = x + dx;
-  if (y_ < 0 || x < 0 || y_ >= grid.length || x_ >= grid[0].length) return false;
-  return grid[y_][x_] < grid[y][x];
+  if (y_ < 0 || x_ < 0 || y_ >= grid.length || x_ >= grid[0].length) return false;
+  const point = grid[y_][x_];
+  if (point === undefined) {
+    console.error(y, x, dy, dx);
+  }
+  debugArr.push(grid[y_][x_]);
+  return grid[y_][x_] <= grid[y][x];
 }
 
 function isLowPoint(y: number, x: number, grid: Grid): boolean {
   const dirs = [{ dy: 0, dx: 1 }, { dy: 1, dx: 0 }, { dy: 0, dx: -1 }, { dy: -1, dx: 0 }];
+  const debugArr = [];
   for (const { dy, dx } of dirs) {
-    if (pointIsLower(y, x, dy, dx, grid)) return false;
+    if (pointIsLowerOrEqual(y, x, dy, dx, grid, debugArr)) return false;
   }
+  console.log(grid[y][x], debugArr, y, x);
   return true;
 }
 function part1() {
@@ -28,6 +35,7 @@ function part1() {
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[0].length; x++) {
       if (isLowPoint(y, x, grid)) {
+        //console.log(y, x);
         riskSum += grid[y][x] + 1;
       }
     }
