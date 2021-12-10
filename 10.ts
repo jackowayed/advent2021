c919355510a93cfeababfa9f12be6506f19d2264
Daniel Jackoway <commits@danieljackoway.com>
2021-12-09 21:20:42 -0800

    10 p2

--- 10.ts
+++ 10.ts
@@ -10,6 +10,19 @@ function matches(open: string, close: string) {
   return open === "(" && close === ")" || open === "[" && close === "]" || open === "{" && close === "}" || open === "<" && close === ">";
 }
 
+function scoreStack(stack: string[]): number {
+  let score = 0;
+  while (stack.length > 0) {
+    const c = stack.pop();
+    score *= 5;
+    if (c === "(") score += 1;
+    else if (c === "[") score += 2;
+    else if (c === "{") score += 3;
+    else if (c === "<") score += 4;
+  }
+  return score;
+}
+
 function scoreLine(line: string): number {
   const stack = [];
   for (const c of [...line]) {
@@ -17,23 +30,15 @@ function scoreLine(line: string): number {
     else {
       const matcher = stack.pop()!;
       if (!matches(matcher, c)) {
-        if (c === ")") return 3;
-        else if (c === "]") return 57;
-        else if (c === "}") return 1197;
-        else if (c === ">") return 25137;
-        else { throw new Error(c); }
+        return 0;
       }
     }
   }
-  return 0;
+  return scoreStack(stack);
 }
 
 function part1() {
   const lines = readFile();
-  let ct = 0;
-  for (const l of lines) ct += scoreLine(l);
-  return ct;
-
 
 
 }
@@ -41,5 +46,15 @@ console.log(part1());
 
 function part2() {
   const lines = readFile();
+  let scores = [];
+  for (const l of lines) {
+    const score = scoreLine(l);
+    if (score > 0) scores.push(score);
+  }
+  // sort
+  scores.sort((a, b) => a - b);
+  console.log(scores);
+  console.log(scores.length, scores.length / 2);
+  return scores[scores.length / 2 - 0.5];
 }
 console.log(part2());
